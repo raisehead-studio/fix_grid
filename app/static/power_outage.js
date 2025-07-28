d6a2e0939bf34e7a808381259aa4ee34ea89a080
Dada Chan <vincent8397@gmail.com>
2025-07-28 19:00:39 +0800

    fix: fix excel count

--- app/static/power_outage.js
+++ app/static/power_outage.js
@@ -436,7 +436,7 @@ function exportToExcel() {
       e.district,
       e.village,
       e.location,
-      e.original_count,  // 使用 original_count 而不是 count
+      e.count,
       e.reason,
       e.contact,
       e.phone,
